94e11605f0daa138af93899b133f2bca19f795ad
Jim.Jiang <jim.jiang@ringcentral.com>
2023-02-01 10:50:55 +0800

    Add handle property

--- src/DotnetDumpMonitor/Models/ProcessDumpInfo.cs
+++ src/DotnetDumpMonitor/Models/ProcessDumpInfo.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using DotnetDumpMonitor.Commons;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +11,15 @@ namespace DotnetDumpMonitor.Models
 {
     public class ProcessDumpInfo
     {
+
         public int ProcessID { get; }
         public string Name { get; }
+        public IntPtr Handle { get; }
 
         public ProcessDumpInfo(int processID, string name)
         {
             ProcessID = processID;
+            Handle = WindowsHelper.GetRootWindowOfProcess(processID);
             Name = name;
         }
 

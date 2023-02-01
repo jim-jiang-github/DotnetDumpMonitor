92af83c54e28c1e58a078d029e7c35e51082318b
Jim.Jiang <jim.jiang@ringcentral.com>
2023-02-01 10:55:20 +0800

    Revert "Add handle property"
    This reverts commit 94e11605f0daa138af93899b133f2bca19f795ad.

--- src/DotnetDumpMonitor/Models/ProcessDumpInfo.cs
+++ src/DotnetDumpMonitor/Models/ProcessDumpInfo.cs
@@ -1,9 +1,6 @@
-﻿using DotnetDumpMonitor.Commons;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,15 +8,12 @@ namespace DotnetDumpMonitor.Models
 {
     public class ProcessDumpInfo
     {
-
         public int ProcessID { get; }
         public string Name { get; }
-        public IntPtr Handle { get; }
 
         public ProcessDumpInfo(int processID, string name)
         {
             ProcessID = processID;
-            Handle = WindowsHelper.GetRootWindowOfProcess(processID);
             Name = name;
         }
 
